bfe355b864a4679b2e60812ef7bfef2f46406bd2
Gian F. Olivieri <gian.olivieri@mediamonks.com>
2020-02-01 11:40:03 -0300

    assets folder

---
+++ src/assets/board/index.ts
@@ -0,0 +1,15 @@
+export const boardAssets = {
+	BLANK: 'assets/board/blank.png',
+	CAPACITOR: 'assets/board/capacitor.png',
+	CAPACITOR_ACTIVE: 'assets/board/capacitor_active.png',
+	CURVE: 'assets/board/curve.png',
+	CURVE_ACTIVE: 'assets/board/curve.png',
+	DIVIDER: 'assets/board/divider.png',
+	DIVIDER_ACTIVE: 'assets/board/divider-active.png',
+	LINE: 'assets/board/line.png',
+	LINE_ACTIVE: 'assets/board/line-active.png',
+	RESISTOR: 'assets/board/resistor.png',
+	RESISTOR_ACTIVE: 'assets/board/blank.png',
+	USER: 'assets/board/user.png',
+	VOID: 'assets/board/vodi.png'
+};
