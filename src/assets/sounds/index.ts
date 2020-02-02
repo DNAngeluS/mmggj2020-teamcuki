842c44965b2385924f96344fc6bf33be7153e744
Gian F. Olivieri <gian.olivieri@mediamonks.com>
2020-02-02 16:29:55 -0300

    add sounds

--- src/assets/sounds/index.ts
+++ src/assets/sounds/index.ts
@@ -1,4 +1,7 @@
 export const soundAssets = {
-	BACKGROUND_LOOP: '/src/assets/sounds/LVL1-LOOP.wav',
-	FX_SWITCH: '/src/assets/sounds/SWITCH.wav'
+	LEVEL_ONE_LOOP: '/src/assets/sounds/music/level1loop.wav',
+	LEVEL_TWO_LOOP: '/src/assets/sounds/music/level2loop.wav',
+	LEVEL_THREE_LOOP: '/src/assets/sounds/music/level3loop.wav',
+	LEVEL_FOUR_LOOP: '/src/assets/sounds/music/level4loop.wav',
+	FX_SWITCH: '/src/assets/sounds/fx/switch.wav'
 };
--- src/game-objects/sounds/Music.ts
+++ src/game-objects/sounds/Music.ts
@@ -3,32 +3,77 @@ import { soundAssets } from 'assets/sounds';
 import { MediaObjectWithControls } from 'game-objects';
 
 export class Music extends MediaObjectWithControls {
-	public static key = soundAssets.BACKGROUND_LOOP.toString();
+	public static levelOneKey = soundAssets.LEVEL_ONE_LOOP.toString();
+	public static levelTwoKey = soundAssets.LEVEL_TWO_LOOP.toString();
+	public static levelThreeKey = soundAssets.LEVEL_THREE_LOOP.toString();
+	public static levelFourKey = soundAssets.LEVEL_FOUR_LOOP.toString();
 
-	private audio: Phaser.Sound.BaseSound;
+	private audioOne: Phaser.Sound.BaseSound;
+	private audioTwo: Phaser.Sound.BaseSound;
+	private audioThree: Phaser.Sound.BaseSound;
+	private audioFour: Phaser.Sound.BaseSound;
+
+	private loopCounter: number = 0;
 
 	public load = (scene: Phaser.Scene) => {
-		scene.load.audio(Music.key, [soundAssets.BACKGROUND_LOOP]);
+		scene.load.audio(Music.levelOneKey, [soundAssets.LEVEL_ONE_LOOP]);
+		scene.load.audio(Music.levelTwoKey, [soundAssets.LEVEL_TWO_LOOP]);
+		scene.load.audio(Music.levelThreeKey, [soundAssets.LEVEL_THREE_LOOP]);
+		scene.load.audio(Music.levelFourKey, [soundAssets.LEVEL_FOUR_LOOP]);
 	};
 
 	public initialize = (scene: Phaser.Scene) => {
-		this.audio = scene.sound.add(Music.key, { loop: true });
+		this.audioOne = scene.sound.add(Music.levelOneKey, { loop: true });
+		this.audioTwo = scene.sound.add(Music.levelTwoKey, { loop: true });
+		this.audioThree = scene.sound.add(Music.levelThreeKey, { loop: true });
+		this.audioFour = scene.sound.add(Music.levelFourKey, { loop: true });
+
+		this.audioOne.on('loop', this.handleLoop);
+		this.audioTwo.on('loop', this.handleLoop);
+		this.audioThree.on('loop', this.handleLoop);
+		this.audioFour.on('loop', this.handleLoop);
+	};
+
+	public handleLoop = () => {
+		this.loopCounter += 1;
+		console.log(this.loopCounter);
+
+		if (this.loopCounter === 2) {
+			this.audioOne.stop();
+			this.audioTwo.play();
+		}
+
+		if (this.loopCounter === 4) {
+			this.audioTwo.stop();
+			this.audioThree.play();
+		}
+
+		if (this.loopCounter === 6) {
+			this.audioThree.stop();
+			this.audioFour.stop();
+		}
 	};
 
 	public play = () => {
-		if (!this.audio.isPlaying) {
-			this.audio.play();
+		if (!this.audioOne.isPlaying) {
+			this.audioOne.play();
 		}
-		if (this.audio.isPaused) {
-			this.audio.resume();
+		if (this.audioOne.isPaused) {
+			this.audioOne.resume();
 		}
 	};
 
 	public stop = () => {
-		this.audio.stop();
+		this.audioOne.stop();
+		this.audioTwo.stop();
+		this.audioThree.stop();
+		this.audioFour.stop();
 	};
 
 	public pause = () => {
-		this.audio.pause();
+		this.audioOne.pause();
+		this.audioTwo.pause();
+		this.audioThree.pause();
+		this.audioFour.pause();
 	};
 }
