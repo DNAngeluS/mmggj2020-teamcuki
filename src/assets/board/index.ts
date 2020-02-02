d25ee906b54aa2c4a0dd8f630080deb0a55c10c3
Gian F. Olivieri <gian.olivieri@mediamonks.com>
2020-02-02 18:49:26 -0300

    changed URLs

--- src/assets/board/index.ts
+++ src/assets/board/index.ts
@@ -1,14 +1,15 @@
 export const boardAssets = {
-	BLANK: '/src/assets/board/blank.png',
-	CAPACITOR: '/src/assets/board/capacitor.png',
-	CAPACITOR_ACTIVE: '/src/assets/board/capacitor_active.png',
-	CURVE: '/src/assets/board/curve.png',
-	DIVIDER: '/src/assets/board/divider.png',
-	DIVIDER_ACTIVE: '/src/assets/board/divider-active.png',
-	LINE: '/src/assets/board/line.png',
-	RESISTOR: '/src/assets/board/resistor.png',
-	RESISTOR_ACTIVE: '/src/assets/board/blank.png',
-	USER: '/src/assets/board/user.png',
-	VOID: '/src/assets/board/void.png',
-	BG: '/src/assets/board/bg1.png'
+	BLANK: '../src/assets/board/blank.png',
+	CAPACITOR: '../src/assets/board/capacitor.png',
+	CAPACITOR_ACTIVE: '../src/assets/board/capacitor_active.png',
+	CURVE: '../src/assets/board/curve.png',
+	DIVIDER: '../src/assets/board/divider.png',
+	DIVIDER_ACTIVE: '../src/assets/board/divider-active.png',
+	LINE: '../src/assets/board/line.png',
+	RESISTOR: '../src/assets/board/resistor.png',
+	RESISTOR_ACTIVE: '../src/assets/board/blank.png',
+	USER: '../src/assets/board/user.png',
+	VOID: '../src/assets/board/void.png',
+	BG: '../src/assets/board/bg1.png',
+	CREDITS: '../src/assets/board/credits-and-keys.png'
 };
